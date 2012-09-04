54361e57a66a47a3bf32595f801807194384f44b
Dan Liew <delcypher@gmail.com>
2012-09-04 10:31:12 +0100

    Improved extract-times shell script so that it will produce a "all-runtimes.txt" script containing the columns of the <solver>-runtimes.txt files pasted together.

--- scripts/extract-times.sh
+++ scripts/extract-times.sh
@@ -34,6 +34,9 @@ if [ -z "${SOLVERS}" ]; then
 	exit
 fi
 
+MERGE_FILE="${OUTPUT_DIR}/all-runtimes.txt"
+MERGE_TEMP_FILE="${OUTPUT_DIR}/merge-runtimes-temp.txt"
+MERGE_CMD_LINE=""
 #Loop over solvers extracting info
 for solver in ${SOLVERS}
 do
@@ -45,4 +48,20 @@ echo -e "#[Run time]\t[ satisfiability]" > "${OUTPUT_FILE}"
 cat "${INPUT}" | awk 'BEGIN { OFS="\t";}
 		     /^'${solver}'/ { print $2,$3;} ' >> "${OUTPUT_FILE}"
 
+#Output part of header for merge file
+echo -en "[Solver ${solver}]\t\t" >> "${MERGE_TEMP_FILE}"
+
+#Add solver to the merge command line
+MERGE_CMD_LINE="${MERGE_CMD_LINE} ${OUTPUT_FILE}"
+
 done
+
+#Output new line for merge file
+echo "" >> "${MERGE_TEMP_FILE}"
+
+echo "Creating ${MERGE_FILE}"
+#Create merged file
+cat "${MERGE_TEMP_FILE}" <(paste ${MERGE_CMD_LINE}) > "${MERGE_FILE}"
+
+#Remove the temp file
+rm "${MERGE_TEMP_FILE}"
